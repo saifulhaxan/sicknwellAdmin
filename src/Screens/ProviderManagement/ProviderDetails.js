fc48b1768a7e8492055540053a26889262d7c434
MUmairqureshi <umairqadeer586@gmail.com>
2024-02-28 16:02:36 +0500

    Add Provider Data

--- src/Screens/ProviderManagement/ProviderDetails.js
+++ src/Screens/ProviderManagement/ProviderDetails.js
@@ -15,7 +15,13 @@ import { useParams } from "react-router-dom";
 import { DashboardLayout } from "../../Components/Layout/DashboardLayout";
 import BackButton from "../../Components/BackButton";
 import CustomModal from "../../Components/CustomModal";
+import CustomButton from "../../Components/CustomButton";
+import CustomInput from "../../Components/CustomInput"
+import CustomTable from "../../Components/CustomTable"
 import { BASE_URL } from "../../Api/apiConfig";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV, faPencil } from "@fortawesome/free-solid-svg-icons";
+import { Dropdown } from "react-bootstrap";
 
 const ProviderDetails = () => {
 
@@ -27,18 +33,13 @@ const ProviderDetails = () => {
   const [profileData, setProfileData] = useState({});
 
   const [showModal, setShowModal] = useState(false);
-  const [showModal2, setShowModal2] = useState(false);
-  const [showModal3, setShowModal3] = useState(false);
-  const [showModal4, setShowModal4] = useState(false);
+  const [addProduct, setAddProduct] = useState(false);
+  const [productData, setProductData] = useState();
+  const [formData, setFormData] = useState({
+    provider: id,
+    description: 'Dental'
+  });
 
-  const inActive = () => {
-    setShowModal(false)
-    setShowModal2(true)
-  }
-  const Active = () => {
-    setShowModal3(false)
-    setShowModal4(true)
-  }
 
   useEffect(() => {
     document.querySelector('.loaderBox').classList.remove("d-none");
@@ -67,6 +68,92 @@ const ProviderDetails = () => {
       })
   }, [id]);
 
+  const handleSubmit = event => {
+    event.preventDefault()
+    document.querySelector('.loaderBox').classList.remove('d-none')
+
+    // Create a new FormData object
+    const formDataMethod = new FormData()
+    for (const key in formData) {
+      formDataMethod.append(key, formData[key])
+    }
+
+    // Make the fetch request
+    fetch(`${BASE_URL}api/v1/provider_service/`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Token ${LogoutData}`
+      },
+      body: formDataMethod // Use the FormData object as the request body
+    })
+      .then(response => {
+        document.querySelector('.loaderBox').classList.add('d-none')
+        return response.json()
+      })
+      .then(data => {
+        document.querySelector('.loaderBox').classList.add('d-none')
+        console.log(data);
+        setShowModal(false);
+        setAddProduct(true);
+        setTimeout(() => {
+          setAddProduct(false);
+        }, 1000);
+        
+        getProductList()
+
+      })
+  }
+
+  const getProductList = event => {
+    document.querySelector('.loaderBox').classList.remove('d-none')
+
+    // Create a new FormData object
+    const formDataMethod = new FormData()
+    for (const key in formData) {
+      formDataMethod.append(key, formData[key])
+    }
+
+    // Make the fetch request
+    fetch(`${BASE_URL}api/v1/provider_service/`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Token ${LogoutData}`
+      },
+    })
+      .then(response => {
+        document.querySelector('.loaderBox').classList.add('d-none')
+        return response.json()
+      })
+      .then(data => {
+        document.querySelector('.loaderBox').classList.add('d-none')
+        console.log(data);
+        setProductData(data)
+
+
+      })
+  }
+
+  useEffect(() => {
+    getProductList()
+  }, [])
+
+
+  const productHeader = [
+    {
+      key: 'name',
+      title: 'Product Name',
+    },
+    {
+      key: 'price',
+      title: 'Price Name',
+    },
+    {
+      key: 'action',
+      title: 'Action',
+    }
+  ]
 
   return (
     <>
@@ -118,15 +205,96 @@ const ProviderDetails = () => {
                   </div>
                 </div>
               </div>
+              <div className="row justify-content-end">
+                <div className="col-md-3">
+                  <CustomButton variant='primaryButton' text='Add Product' type='button' onClick={() => { setShowModal(true) }} />
+                </div>
+              </div>
+              {productData && productData.length > 0 ? (
+                <div className="row mb-3 textLeft">
+                  <div className="col=md-12">
+                    <h2 className="mainTitle">
+                      Price List
+                    </h2>
+                  </div>
+                  <div className="col-12">
+                    <CustomTable
+                      headers={productHeader}
+
+                    >
+                      <tbody>
+                        {productData?.map((item, index) => (
+                          <tr key={index}>
+                            <td className="text-capitalize">
+                              {item.name}
+                            </td>
+                            <td className="text-capitalize">
+                              {`$ ${item.price}`}
+                            </td>
+                            <td>
+                              <Dropdown className="tableDropdown">
+                                <Dropdown.Toggle variant="transparent" className="notButton classicToggle">
+                                  <FontAwesomeIcon icon={faEllipsisV} />
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu align="end" className="tableDropdownMenu">
+                                  <button onClick={() => {
+                                    // brandID(item.id)
+                                    // setUserFrom(true)
+                                  }} className="tableAction"><FontAwesomeIcon icon={faPencil} className="tableActionIcon" />Edit</button>
+                                </Dropdown.Menu>
+                              </Dropdown>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </CustomTable>
+                  </div>
+                </div>
+              ) : ''}
+
             </div>
           </div>
         </div>
 
-        <CustomModal show={showModal} close={() => { setShowModal(false) }} action={inActive} heading='Are you sure you want to mark this user as inactive?' />
-        <CustomModal show={showModal2} close={() => { setShowModal2(false) }} success heading='Marked as Inactive' />
+        <CustomModal show={showModal} close={() => { setShowModal(false) }} >
+          <CustomInput
+            label="Add Product Name"
+            type="text"
+            placeholder="Add Product Name"
+            required
+            name="name"
+            labelClass='mainLabel'
+            inputClass='mainInput'
+            onChange={(event) => {
+              setFormData({ ...formData, name: event.target.value });
+              console.log(formData);
+            }}
 
-        <CustomModal show={showModal3} close={() => { setShowModal3(false) }} action={Active} heading='Are you sure you want to mark this user as Active?' />
-        <CustomModal show={showModal4} close={() => { setShowModal4(false) }} success heading='Marked as Active' />
+          />
+
+          <CustomInput
+            label="Add Product Price"
+            type="text"
+            placeholder="Add Product Price"
+            required
+            name="price"
+            labelClass='mainLabel'
+            inputClass='mainInput'
+            onChange={(event) => {
+              setFormData({ ...formData, price: event.target.value });
+              console.log(formData);
+            }}
+
+          />
+          <CustomButton variant='primaryButton' text='Add' type='button' onClick={handleSubmit} />
+        </CustomModal>
+
+        <CustomModal show={addProduct} close={() => { setAddProduct(false) }} success heading='Product Added Successfully!' />
+        {/* <CustomModal show={showModal} close={() => { setShowModal(false) }} action={inActive} heading='Are you sure you want to mark this user as inactive?' />
+        
+
+          <CustomModal show={showModal3} close={() => { setShowModal3(false) }} action={Active} heading='Are you sure you want to mark this user as Active?' />
+          <CustomModal show={showModal4} close={() => { setShowModal4(false) }} success heading='Marked as Active' /> */}
       </DashboardLayout>
     </>
   );
